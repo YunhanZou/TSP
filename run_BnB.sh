69f7ad508cd392618f263b4cf88b008bc6c2acfd
吴侠 <wxflawless1223@gmail.com>
2018-11-19 21:42:08 -0500

    update fodler name

--- run_BnB.sh
+++ run_BnB.sh
@@ -1,5 +1,5 @@
 
 for filename in DATA/*.tsp; do
     # echo $filename
-    python src/Main.py --inst $filename --alg BnB --time 100
+    python code/Main.py --inst $filename --alg BnB --time 100
 done
