a893ae25eddbfa4038d923d7737c167570a4c22a
IppikiSlime <lhh13fall@gmail.com>
2018-11-25 16:33:28 -0500

    Two opt...still have some bugs to check

--- code/LocalSearch.py
+++ code/LocalSearch.py
@@ -66,7 +66,7 @@ class TwoOpt:
             i += 1
             j -= 1
 
-    def eval_path(self):
+    def eval_initial_path(self):
         """
         Evaluate the length of current path.
         Iterative query the distance between adjacent cities in self.cost_matrix and add to the total distance.
@@ -75,35 +75,76 @@ class TwoOpt:
         """
         dist = 0
         for i in range(0, self.n):
-            print self.dist_matrix[self.path[i]][self.path[i + 1]]
+            # print self.dist_matrix[self.path[i]][self.path[i + 1]]
             dist += self.dist_matrix[self.path[i]][self.path[i + 1]]
         return dist
 
-    def local_search(self):
+    def eval_try_path(self, old_quality, i, j):
+        """
+        Input: Old path quality, int i,  int j, j > i
+        Output: Tentative path quality if we 2-opt index i and j
+        Evaluate the length of path after swap i, j.
+        Note that we don't need to re-calculate the entire path; 
+        just break the path on i-1~i and j~j+1, and add the length of i-1~j and i~j+1.
+        """
+        # print old_quality, i, j
+        # print self.path
+        # self.swap(i, j)
+        # print self.path
+        # print self.eval_initial_path()
+        # self.swap(i, j)
+        # print "Is my path correct?"
+        # print self.path[i-1], self.path[i], self.dist_matrix[self.path[i-1]][self.path[i]]
+        # print self.path[j], self.path[j+1], self.dist_matrix[self.path[j]][self.path[j+1]]
+        # print self.path[i-1], self.path[j], self.dist_matrix[self.path[i-1]][self.path[j]]
+        # print self.path[i], self.path[j+1], self.dist_matrix[self.path[i]][self.path[j+1]]
+        new_quality = old_quality
+        new_quality -= self.dist_matrix[self.path[i-1]][self.path[i]]
+        new_quality -= self.dist_matrix[self.path[j]][self.path[j+1]]
+        new_quality += self.dist_matrix[self.path[i-1]][self.path[j]]
+        new_quality += self.dist_matrix[self.path[i]][self.path[j+1]]
+        return new_quality
+
+    def two_opt(self):
+        """
+        Output: best quality (minimum path length found), path, duration of this run 
+        Body of two-opt local search algorithm.
+        Starting from the begnning of path, iterate through all possible swap and see if an improvement can be made.
+        Terminate when no improvement can be made, or no time left.
+        Note that this method might get stuck in local minimum.
+        """
         curr_time = int(round(time.time() * 1000))
         duration = curr_time - self.start_time
-        best_quality = self.eval_path()
+        best_quality = self.eval_initial_path()
+        try_quality = best_quality
 
         # Stop condition:
-        # 1. No improvement can be made.
-        # 2. We are running out of time ... think about how to add this later.
+        # 1. No improvement can be made in an iteration that goes through the entire path
+        # 2. We are running out of time 
 
         while self.time_limit - duration > 1:
-            for i in range(0, self.n):
-                for j in range(i + 1, self.n + 1):
-                    self.swap(i, j)
-                    try_quality = self.eval_path()
+            can_improve = False
+            for i in range(0, self.n-1):
+                for j in range(i + 1, self.n):
+                    try_quality = self.eval_try_path(best_quality, i, j)
                     if try_quality < best_quality:
                         best_quality = try_quality
+                        self.swap(i, j)
+                        can_improve = True
                         break
+                if can_improve:
+                    break
+            if not can_improve:
+                break
 
+        return best_quality, self.path, duration
 
 def test_initialize():
     dist_matrix = np.array([[0, 20, 30, 10, 11],
-                            [15, 0, 16, 4, 2],
-                            [3, 5, 0, 2, 4],
-                            [19, 6, 18, 0, 3],
-                            [16, 4, 7, 16, 0]])
+                            [20, 0, 16, 4, 2],
+                            [30, 16, 0, 2, 4],
+                            [10, 4, 2, 0, 3],
+                            [11, 2, 4, 3, 0]])
 
     time_limit = 1
 
@@ -121,10 +162,20 @@ def test_swap():
     print topt1.path
 
 
-def test_eval_path():
+def test_eval_initial_path():
     topt1 = test_initialize()
-    print topt1.eval_path()
+    print topt1.eval_initial_path()
+
+
+def test_eval_try_path():
+    topt1 = test_initialize()
+    old_quality = topt1.eval_initial_path()
+    print topt1.eval_try_path(old_quality, 3, 4)
+
+def test_two_opt():
+    topt1 = test_initialize()
+    print topt1.two_opt()
 
 
 if __name__ == "__main__":
-    print test_eval_path()
+    test_two_opt()
