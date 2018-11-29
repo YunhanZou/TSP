a2d4f1672649280e62d4d016d4abf16300a303a6
吴侠 <wxflawless1223@gmail.com>
2018-11-29 11:32:23 -0500

    add results and running script for local search

---
+++ run_LS.sh
@@ -0,0 +1,6 @@
+
+for filename in DATA/*.tsp; do
+    # echo $filename
+    python code/Main.py --inst $filename --alg LS1 --time 600 --seed 12345
+    python code/Main.py --inst $filename --alg LS2 --time 600
+done
