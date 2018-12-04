4a566670f941878f8b8c3b260c38c6b10922b3b0
吴侠 <wxflawless1223@gmail.com>
2018-12-04 11:37:23 -0500

    update ls1

--- run_LS2.sh
+++ run_LS2.sh
@@ -1,14 +1,5 @@
 
 for filename in DATA/*.tsp; do
     # echo $filename
-    python code/Main.py --inst $filename --alg LS1 --time 600 --seed 12345
-    python code/Main.py --inst $filename --alg LS1 --time 600 --seed 35313
-    python code/Main.py --inst $filename --alg LS1 --time 600 --seed 45631
-    python code/Main.py --inst $filename --alg LS1 --time 600 --seed 90593
-    python code/Main.py --inst $filename --alg LS1 --time 600 --seed 2935812
-    python code/Main.py --inst $filename --alg LS1 --time 600 --seed 2523643
-    python code/Main.py --inst $filename --alg LS1 --time 600 --seed 3496034
-    python code/Main.py --inst $filename --alg LS1 --time 600 --seed 425
-    python code/Main.py --inst $filename --alg LS1 --time 600 --seed 634506
-    python code/Main.py --inst $filename --alg LS1 --time 600 --seed 540963
+    python code/Main.py --inst $filename --alg LS2 --time 600 --seed 425
 done
